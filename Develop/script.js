07a128ae9a07e439b0ae5fcb8d4a29863ed2dfd0
JagpreetRandio <115323708+JagpreetRandio@users.noreply.github.com>
2022-11-28 13:08:01 -0800

    fixed code and its working

--- Develop/script.js
+++ Develop/script.js
@@ -22,29 +22,31 @@ function timeBlockColor() {
 
   $(".time-block").each(function() {
       var currentHour = parseInt($(this).attr("id")); 
+      if (hour > currentHour){
+        $(this).addClass("past");
+        
+    } else if (hour === currentHour){
+        $(this).addClass("present");
+        $(this).removeClass("past");
 
-      if (currentHour > hour) {
-          $(this).addClass("future");
-      } else if (currentHour === hour) {
-          $(this).addClass("present");
-      } else {
-          $(this).addClass("past");
-      }
+    } else {$(this).addClass("future");
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+    }
   })
 };
 timeBlockColor();
 
-
 //Information stays on the page when refreshed
-$("#hour-9 .description").val(localStorage.getItem("hour-9"));
-$("#hour-10 .description").val(localStorage.getItem("hour-10"));
-$("#hour-11 .description").val(localStorage.getItem("hour-11"));
-$("#hour-12 .description").val(localStorage.getItem("hour-12"));
-$("#hour-13 .description").val(localStorage.getItem("hour-13"));
-$("#hour-14 .description").val(localStorage.getItem("hour-14"));
-$("#hour-15 .description").val(localStorage.getItem("hour-15"));
-$("#hour-16 .description").val(localStorage.getItem("hour-16"));
-$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
 
 
 });
